53c42db84b30b364c5da06512b4c0ee49aba4a44
jasonodoom <jason@ohdoom.net>
2018-09-23 05:13:32 -0400

    wip

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -8,6 +10,40 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = Song.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name  = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    return song
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |track|
+      track.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name)
+    self.create_by_name(name)
+    return name
+    if self.name != self.find_by_name(name)
+      self.create_by_name(name)
+    end
   end
 
 end
