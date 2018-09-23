4456bd7e03e42720865cd3deae42b35c8642e3c6
jasonodoom <jason@ohdoom.net>
2018-09-23 12:11:50 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -40,10 +40,32 @@ class Song
   def self.find_or_create_by_name(name)
     self.find_by_name(name)
     self.create_by_name(name)
-    return name
-    if self.name != self.find_by_name(name)
-      self.create_by_name(name)
+    return self.find_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by do |track|
+      track.name
     end
+  end
+
+  def self.new_from_filename(name)
+    trackName = name.split("-").last.split(".mp3")
+    fmtTrack=trackName.first.strip
+    artistName = name.split("-").first.strip
+
+    song = self.new
+    song.name = fmtTrack
+    song.artist_name = artistName
+    return song
+  end
+
+   def self.create_from_filename(name)
+     @@all << self.new_from_filename(name)
+   end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
